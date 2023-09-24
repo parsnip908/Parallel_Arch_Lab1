839e21dde95904fee30cab06737548c5bfeec7a8
giovannijana <60889211+giovannijana@users.noreply.github.com>
2023-09-24 14:12:06 -0500

    Create CILK_script.sh

---
+++ CILK_script.sh
@@ -0,0 +1,31 @@
+# Notes:
+#
+#   -- Launch this script by executing
+#      "sbatch milan.mpi.slurm" on a Lonestar6 login node.
+#
+#   -- Use ibrun to launch MPI codes on TACC systems.
+#      Do NOT use mpirun or mpiexec.
+#
+#   -- Max recommended MPI ranks per Milan node: 128
+#      (start small, increase gradually).
+#
+#   -- If you're running out of memory, try running
+#      fewer tasks per node to give each task more memory.
+#
+#----------------------------------------------------
+
+#SBATCH -J CILK_mm           # Job name
+#SBATCH -o CILK_mm.o%j       # Name of stdout output file
+#SBATCH -e CILK_mm.e%j       # Name of stderr error file
+#SBATCH -p normal          # Queue (partition) name
+#SBATCH -N 1               # Total # of nodes
+#SBATCH -n 128              # Total # of mpi tasks
+#SBATCH -t 00:30:00        # Run time (hh:mm:ss)
+#SBATCH --mail-type=all    # Send email at begin and end of job
+#SBATCH --mail-user=username@tacc.utexas.edu
+
+# Any other commands must follow all #SBATCH directives...
+/work/08382/mengtian/ls6/cilk/bin/clang -fopencilk -O3 -fno-tree-vectorize -mno-avx -mno-avx2 -mno-mmx -mno-fma -mno-sse3 -mno-sse4 -mno-sse4.1 -mno-sse4.2 fib.c -o fib
+
+# Launch CILK code...
+./fib 40

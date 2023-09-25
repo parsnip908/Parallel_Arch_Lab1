9da253e244c666ce6a3c5e84c87d281c002f16af
parsnip908 <pkharche@ucdavis.edu>
2023-09-25 11:38:16 -0500

    Update CILK_mm.c

--- CILK_mm.c
+++ CILK_mm.c
@@ -20,24 +20,36 @@ int main(int argc, char** argv)
     matrix_dimension_size = atoi(argv[3]);
     num_arg_matrices = init_gen_sub_matrix(test_set);
 
-
-    double **arg_matrices;
     double *result;
-    double *output_matrix;
 
     //alloc space for all arg matrices
-    arg_matrices = (double **)my_malloc(sizeof(double*) * num_arg_matrices *);
-    output_matrix = (double *)my_malloc(sizeof(double) * matrix_dimension_size * matrix_dimension_size);
-    //gen matricies
+    int matrix_size = matrix_dimension_size * matrix_dimension_size;
+    double *arg_matrices = (double *)my_malloc(sizeof(double*) * num_arg_matrices * matrix_size);
+    double *output_matrix = (double *)my_malloc(sizeof(double) * matrix_size);
+    double *row_matrix = arg_matrices;
+    double *column_matrices = arg_matrices + matrix_size;
     
     //Generating arg matrices
-    for(i=0; i < num_arg_matrices-1; i++){
-          arg_matrices[i] = (double *)my_malloc(sizeof(double) * matrix_dimension_size * matrix_dimension_size);
-          if (gen_sub_matrix(0, test_set, i, arg_matrices[i], 0, matrix_dimension_size-1, 1, 0, matrix_dimension_size - 1, 1, 1) == NULL) {
-                  printf("inconsistency in gen_sub_matrix\n");
-                  exit(1);
-          }
-      }
+    if (gen_sub_matrix(0, test_set, i, &arg_matrices[0], 0, matrix_dimension_size-1, 1, 0, matrix_dimension_size - 1, 1, 1) == NULL) 
+    {
+        printf("inconsistency in gen_sub_matrix\n");
+        exit(1);
+    }
+    for(i=1; i < num_arg_matrices; i++)
+    {
+        // arg_matrices[i] = (double *)my_malloc(sizeof(double) * matrix_dimension_size * matrix_dimension_size);
+        if (gen_sub_matrix(0, test_set, i, &arg_matrices[i*matrix_size], 0, matrix_dimension_size-1, 1, 0, matrix_dimension_size - 1, 1, 0) == NULL) 
+        {
+            printf("inconsistency in gen_sub_matrix\n");
+            exit(1);
+        }
+    }
+
+    //print everything
+
+    //multiply
+
+    //print result
 
     return 0;
 }
